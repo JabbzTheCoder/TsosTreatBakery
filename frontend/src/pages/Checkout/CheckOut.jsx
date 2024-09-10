b7ad4d8e929759ad2fb11376e33f4e5830047878
JabbzTheCoder <jabbzthecoder@gmail.com>
2024-09-10 02:27:02 +0200

    Frontend done!!!!!!

--- frontend/src/pages/Checkout/CheckOut.jsx
+++ frontend/src/pages/Checkout/CheckOut.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import './CheckOut.css'
+import { StoreContext } from '../../context/StoreContext'
 const CheckOut = () => {
+  
+  const {getTotalCartAmount} = useContext(StoreContext);
+    
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className='place-order-left'>
+        <p className='title'>Delivery Information</p>
+
+        <div className='multi-fields'>
+          <input type='text' placeholder='First name' />
+          <input type='text' placeholder='Last name' />
+        </div>
+        <input type='email' placeholder='Email address' />
+        <input type='text' placeholder='Street' />
+        <div className='multi-fields'>
+          <input type='text' placeholder='City/Town' />
+          <input type='text' placeholder='Province' />
+        </div>
+        <div className='multi-fields'>
+          <input type='text' placeholder='Zip code' />
+          <input type='text' placeholder='Country' />
+        </div>
+        <input type='text' placeholder='Phone' />
+      </div>
+      <div className='place-order-right'>
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className='cart-total-details'>
+              <p>Subtotal</p>
+              <p>R{getTotalCartAmount()}</p>
+            </div>
+            <div className='cart-total-details'>
+              <p>Tax</p>
+              <p>R{2}</p>
+            </div>
+            <div className='cart-total-details'>
+              <b>Total</b>
+              <b>R{getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>Proceed to Payment</button>
+        </div>
+      </div>
+    </form> 
   )
 }
 
